ea94e152924f2dda5d4e75bb312bec1ea294ae8e
Jonathan Piat <jonathan.piat@qapa.com>
2014-08-02 00:36:54 +0200

    Version 0.2

--- pgwbrowser.js
+++ pgwbrowser.js
@@ -12,19 +12,25 @@
         var pgwBrowser = {};        
         pgwBrowser.userAgent = navigator.userAgent;
         pgwBrowser.browser = {};
+        pgwBrowser.window = {};
         pgwBrowser.os = {};
 
+        // The order of this array is important, be careful if you change it.
         var browserData = [
-            { name: 'Chrome',  identifier: 'Chrome/([0-9.]*)',   versionSeparator: '.' },
-            { name: 'Chrome',  identifier: 'Chromium/([0-9.]*)', versionSeparator: '.' },
-            { name: 'Firefox', identifier: 'Firefox/([0-9.]*)',  versionSeparator: '.' },
-            { name: 'Safari',  identifier: 'Safari/([0-9.]*)',   versionSeparator: '.' },
-            { name: 'MSIE',    identifier: 'MSIE/([0-9.]*)',     versionSeparator: '.' },
-            { name: 'Trident', identifier: 'Trident/([0-9.]*)',  versionSeparator: '.' },
-            { name: 'Opera',   identifier: 'Opera/([0-9.]*)',    versionSeparator: '.' }
+            { name: 'Chrome',   identifier: 'Chrome/([0-9.]*)'     },
+            { name: 'Chrome',   identifier: 'Chromium/([0-9.]*)'   },
+            { name: 'Firefox',  identifier: 'Firefox/([0-9.]*)'    },
+            { name: 'Opera',    identifier: 'Opera ([0-9.]*)'      },
+            { name: 'Opera',    identifier: 'Opera/([0-9.]*)',     versionIdentifier: 'Version/([0-9.]*)' },
+            { name: 'Safari',   identifier: 'Safari/([0-9.]*)',    versionIdentifier: 'Version/([0-9.]*)' },              
+            { name: 'Explorer', identifier: 'MSIE ([a-zA-Z0-9.]*)' },
+            { name: 'Explorer', identifier: 'Trident/([0-9.]*)',   versionIdentifier: 'rv:([0-9.]*)' }
         ];
 
-        var osData = [
+        /*var osData = [
+            { name: 'Android',                group: 'android', identifier: 'Android',                 versionSeparator: null },
+            { name: 'iPhone',                 group: 'ios',     identifier: 'iPhone OS',               versionSeparator: null },
+            { name: 'iPad',                   group: 'ios',     identifier: 'iPad',                    versionSeparator: null },
             { name: 'Windows 2000',           group: 'windows', identifier: 'Windows NT 5.0',          versionSeparator: null },
             { name: 'Windows XP',             group: 'windows', identifier: 'Windows NT 5.1',          versionSeparator: null },
             { name: 'Windows Vista',          group: 'windows', identifier: 'Windows NT 6.0',          versionSeparator: null },
@@ -45,27 +51,70 @@
             { name: 'Debian',                 group: 'linux',   identifier: 'Debian',                  versionSeparator: null },
             { name: 'Gentoo',                 group: 'linux',   identifier: 'Gentoo',                  versionSeparator: null },
             { name: 'Linux',                  group: 'linux',   identifier: 'Linux',                   versionSeparator: null }
-        ];
+        ];*/
 
+        
+        
+        //pgwBrowser.userAgent = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36';
+        //pgwBrowser.userAgent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; da-dk) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1';
+        //pgwBrowser.userAgent = 'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25';
+        //pgwBrowser.userAgent = 'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 6.0)';
+        //pgwBrowser.userAgent = 'Mozilla/4.0 (compatible; MSIE 5.5b1; Mac_PowerPC)';
+        pgwBrowser.userAgent = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko ';
+        pgwBrowser.userAgent = 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14';
+        pgwBrowser.userAgent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14';
+        
+        
+        
         //  Set browser
-        var setBrowserData = function() {
+        var setBrowser = function() {
+            var userAgent = pgwBrowser.userAgent.toLowerCase();
+            
+            // Check browser type
             for (i in browserData) {
-                var regExp = new RegExp(browserData[i].identifier);
-                var regExpResult = regExp.exec(pgwBrowser.userAgent);
+                var browserRegExp = new RegExp(browserData[i].identifier.toLowerCase());
+                var browserRegExpResult = browserRegExp.exec(userAgent);
                 
-                if (regExpResult != null && regExpResult[1]) {
-                    getVersion(regExpResult[1], browserData[i].versionSeparator);
+                if (browserRegExpResult != null && browserRegExpResult[1]) {
+                    pgwBrowser.browser.name = browserData[i].name;
+                
+                    // Check version
+                    if (browserData[i].versionIdentifier) {
+                        var versionIdentifier = browserData[i].versionIdentifier.toLowerCase();
+                        var versionRegExp = new RegExp(versionIdentifier);
+                        var versionRegExpResult = versionRegExp.exec(userAgent);
+                        
+                        if (versionRegExpResult != null && versionRegExpResult[1]) {
+                            setVersion(versionRegExpResult[1], '.');
+                        }
+                    } else {
+                        setVersion(browserRegExpResult[1], '.');
+                    }
+                    
+                    break;
                 }
             }
         };
         
         // Get version
-        var getVersion = function(version, separator) {
-            console.log(version, separator);
+        var setVersion = function(version, separator) {
+            pgwBrowser.browser.fullVersion = version;
+
+            var splitVersion = version.split(separator, 2);
+
+            // Major version
+            if (splitVersion[0]) {
+                pgwBrowser.browser.majorVersion = parseInt(splitVersion[0]);
+            }
+
+            // Minor version
+            if (splitVersion[1]) {
+                pgwBrowser.browser.minorVersion = parseInt(splitVersion[1]);
+            }
         };
       
         // Set data
-        setBrowserData();
+        setBrowser();
         
         return pgwBrowser;
     }
